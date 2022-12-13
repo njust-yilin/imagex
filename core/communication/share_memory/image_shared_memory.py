4977977df7d64d07fa1daf5a98bc0e551d70c969
yilin <1101510422@njust.edu.cn>
2022-12-13 13:50:03 +0800

    update shared memory image

--- core/communication/share_memory/image_shared_memory.py
+++ core/communication/share_memory/image_shared_memory.py
@@ -2,14 +2,16 @@ import numpy as np
 from multiprocessing import shared_memory, Lock
 from typing import Tuple
 from loguru import logger
+
 from core.utils import get_timestamp_ms
 
 
 class ImageSharedMemory(object):
-    def __init__(self, name:str, image_hwc:Tuple, image_count:int,):
+    def __init__(self, name:str, image_hwc:Tuple[int, int, int], image_count:int):
         self.image_count = image_count
         self.name = name
         self.image_hwc = image_hwc
+
         self.lock = Lock()
         self.index = 0
 
@@ -25,13 +27,18 @@ class ImageSharedMemory(object):
         self.use_count = np.ndarray((1,), dtype=np.uint32, buffer=self.use_count_shared_memory.buf)
         self.use_count[0] = 0
 
-    @logger.catch
-    def free_all(self):
-        with self.lock:
-            empty_images = np.zeros(self.shape, dtype=np.uint8)
-            self.images = empty_images.data
-            logger.info(f"Free all images[{self.name}]")
-            self.use_count[0] = 0
+    def init_shared_memory(self, name, size):
+        logger.info(f"Initializing shared memory {name}, size={size}")
+        try:
+            sm = shared_memory.SharedMemory(name=name, size=size, create=False)
+            return sm
+        except FileNotFoundError as e:
+            logger.warning(e)
+            sm = shared_memory.SharedMemory(name=name, size=size, create=True)
+            return sm
+        except FileExistsError as e:
+            logger.warning(e)
+            return sm
 
     @logger.catch
     def free(self, index:int):
@@ -44,6 +51,25 @@ class ImageSharedMemory(object):
             self.images[index] = empty_image.data
             self.use_count[0] -= 1
             logger.info(f"Free image-{index}[{self.name}], use count={self.use_count[0]}")
+
+    @logger.catch
+    def free_all(self):
+        with self.lock:
+            empty_images = np.zeros(self.shape, dtype=np.uint8)
+            self.images = empty_images.data
+            logger.info(f"Free all images[{self.name}]")
+            self.use_count[0] = 0
+
+    def __del__(self):
+        self.images_shared_memory.close()
+        self.use_count_shared_memory.close()
+        logger.info(f"Closed images shared memory {self.name}")
+
+
+class ImageSharedMemoryProducer(ImageSharedMemory):
+    def __init__(self, name:str, image_hwc:Tuple[int, int, int], image_count:int):
+        super().__init__(name, image_hwc, image_count)
+        self.free_all()
 
     def put(self, image:np.ndarray)->int:
         if self.use_count[0] >= self.image_count:
@@ -58,6 +84,22 @@ class ImageSharedMemory(object):
             logger.info(f"Put image-{self.index}[{self.name}], use count={self.use_count[0]}, next_index={self.index}, took: {get_timestamp_ms()-ts}ms")
         return current_index
 
+    def __del__(self):
+        super().__del__()
+        try:
+            self.images_shared_memory.unlink()
+            logger.info(f"Unlinked images shared memory {self.name}")
+        except: pass
+        try:
+            self.use_count_shared_memory.unlink()
+            logger.info(f"Unlinked use count shared memory {self.name}")
+        except: pass
+
+
+class ImageSharedMemoryConsumer(ImageSharedMemory):
+    def __init__(self, name:str, image_hwc:Tuple[int, int, int], image_count:int):
+        super().__init__(name, image_hwc, image_count)
+
     def get(self, index:int) -> np.ndarray:
         if index < 0 or index >= self.image_count:
             raise ValueError("Index out of range")
@@ -68,53 +110,3 @@ class ImageSharedMemory(object):
             logger.info(f"Get image-{self.name}-{index}, took: {get_timestamp_ms()-ts}ms")
             return image
 
-    def init_shared_memory(self, name, size):
-        logger.info(f"Initializing shared memory {name}, size={size}")
-        try:
-            sm = shared_memory.SharedMemory(name=name, size=size, create=False)
-            return sm
-        except FileNotFoundError as e:
-            logger.warning(e)
-            sm = shared_memory.SharedMemory(name=name, size=size, create=True)
-            return sm
-        except FileExistsError as e:
-            logger.warning(e)
-            return sm
-
-    def cleanup(self):
-        self.images_shared_memory.close()
-        self.use_count_shared_memory.close()
-        logger.info(f"Closed images shared memory {self.name}")
-        try:
-            self.images_shared_memory.unlink()
-            logger.info(f"Unlinked images shared memory {self.name}")
-        except: pass
-        try:
-            self.use_count_shared_memory.unlink()
-            logger.info(f"Unlinked use count shared memory {self.name}")
-        except: pass
-        
-
-if __name__ == "__main__":
-    hwc = (3, 3, 3)
-    p = ImageSharedMemory('test_image', hwc, 10, True)
-    c = ImageSharedMemory('test_image', hwc, 10)
-
-    img = np.random.randint(128, size=hwc, dtype=np.uint8)
-    p.put(img)
-    p.put(img)
-    p.put(img)
-    image = c.get(0)
-    print(image)
-    image[0, 0, 0] = 0
-    print("================================================")
-    image = p.get(0)
-    print(image)
-    print("================================================")
-    p.free(0)
-    p.free(1)
-    p.free(2)
-    image = c.get(0)
-    print(image)
-    c.cleanup()
-    p.cleanup()
