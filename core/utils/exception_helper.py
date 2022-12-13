69d604ee836499e1595bd7a8303dcf3d1d5c706c
yilin <1101510422@njust.edu.cn>
2022-12-13 18:21:15 +0800

    add run_catch_except function

---
+++ core/utils/exception_helper.py
@@ -0,0 +1,13 @@
+from loguru import logger
+
+def run_catch_except(func, args=(), kwargs={}):
+    try:
+        func(*args, **kwargs)
+    except Exception as e:
+        logger.error(e)
+
+if __name__ == '__main__':
+    def test(x, y):
+        print(x, y)
+    run_catch_except(test, kwargs={'x': 1, 'y': 2})
+    run_catch_except(lambda x, y: print(x, y), kwargs={'x': 1, 'y': 2})
